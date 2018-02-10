c0034a0ee18fbd1cb1a60bfea80c5fe0af83a34c
krzysztof <krzysztof.mohr@live.com>
2018-02-10 16:15:10 +0000

    implemented dashboard to display stocks

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import {StockInterface, StocksService} from '../../services/stocks.service';
+import {Subscription} from 'rxjs/Subscription';
 
 @Component({
-  selector: 'app-dashboard',
+  selector: 'dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  stocks: Array<StockInterface>;
+  symbols: Array<string>;
+  private subscription: Subscription;
 
-  constructor() { }
+  constructor(private service: StocksService) {
+    this.symbols = service.get();
+  }
 
   ngOnInit() {
+    this.subscription = this.service.load(this.symbols).subscribe(stocks => this.stocks = stocks);
   }
 
 }
